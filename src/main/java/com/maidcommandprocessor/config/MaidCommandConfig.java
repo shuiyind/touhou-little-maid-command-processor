02c073a788258a26d38a47666695a5c30b0ff4a8
shuiyind <shuiyind@gmail.com>
2026-09-07 18:20:12 +0800

    fix: use 3-param defineList API which is the only available option

--- src/main/java/com/maidcommandprocessor/config/MaidCommandConfig.java
+++ src/main/java/com/maidcommandprocessor/config/MaidCommandConfig.java
@@ -46,21 +46,13 @@ public class MaidCommandConfig {
 
         // Dangerous command blacklist
         builder.comment("Dangerous command blacklist").push("dangerous_commands");
-        List<String> defaultDangerousCommands = new java.util.ArrayList<>();
-        defaultDangerousCommands.add("/kill @a");
-        defaultDangerousCommands.add("/op @a");
-        defaultDangerousCommands.add("/deop @a");
-        defaultDangerousCommands.add("/ban");
-        defaultDangerousCommands.add("/pardon");
-        defaultDangerousCommands.add("/gamemode 3");
-        defaultDangerousCommands.add("/gamemode 0");
-        defaultDangerousCommands.add("/difficulty hard");
-        defaultDangerousCommands.add("/difficulty easy");
-        defaultDangerousCommands.add("/gamerule doDayNightCycle false");
-        defaultDangerousCommands.add("/gamerule keepInventory true");
+        java.util.List<String> defaultDangerousCommands = java.util.Arrays.asList(
+            "/kill @a", "/op @a", "/deop @a", "/ban", "/pardon",
+            "/gamemode 3", "/gamemode 0", "/difficulty hard", "/difficulty easy",
+            "/gamerule doDayNightCycle false", "/gamerule keepInventory true");
         dangerousCommands = builder
             .comment("List of dangerous commands that require higher permission")
-            .defineListInRange("dangerousCommands", defaultDangerousCommands, s -> s instanceof String);
+            .defineList("dangerousCommands", defaultDangerousCommands, s -> s instanceof String);
         minPermissionForDangerous = builder
             .comment("Minimum permission level to execute dangerous commands")
             .defineInRange("minPermissionForDangerous", 2, 1, 3);
